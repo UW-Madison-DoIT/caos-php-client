e98e389d8c726816becdbc730f85dcc06eb03171
Craig Knuth <cknuth@wisc.edu>
2015-07-02 12:14:45 -0500

    Added a Windows check to deal with funky windows file systems when deleting the existing target directory.

--- release-clone.php
+++ release-clone.php
@@ -1,13 +1,13 @@
 <?php 
 
 if (count($argv) !== 3) {
-	echo "Error: Did not pass repository or target directory";
+	echo "Error: Did not pass repository or target directory\n";
 	exit(1);
 }
 
 exec("git --version", $output, $returnval);
 if ($returnval !== 0) {
-	echo "Error: Could not find git!";
+	echo "Error: Could not find git!\n";
 	exit(1);
 }
 unset($output, $returnval);
@@ -17,7 +17,7 @@ $dir = $argv[2];
 
 exec("git clone ".$blessedRepo." ".$dir."/caos-php-client", $output, $returnval);
 if ($returnval !== 0) {
-	echo "Error: Could not clone the blessed repository";
+	echo "Error: Could not clone the blessed repository\n";
 	exit(1);
 }
 unset($output, $returnval);
