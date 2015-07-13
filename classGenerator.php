b0182cbaa3bbd88da30e96995fb0b2182fcf2cc9
Craig Knuth <cknuth@wisc.edu>
2015-07-13 12:17:22 -0500

    Deleted the phing wrapper and modified the README so that users execute vendor/bin/phing directly.
    Also modified the class generator script to accept passing a CAOS API
    version number.

--- classGenerator.php
+++ classGenerator.php
@@ -1,11 +1,18 @@
 <?php 
 
+if ($argc !== 2) {
+	echo "Error: Invalid number of arguments.";
+	exit(1);
+}
+
+$caosVersion = $argv[1];
+
 require_once(__DIR__."/vendor/autoload.php");
 
 $generator = new \Wsdl2PhpGenerator\Generator();
 $generator->generate(
 	new \Wsdl2PhpGenerator\Config(array(
-			'inputFile' => 'http://esb.services.wisc.edu/esbv2/CHUB/WebService/chub-ws-1.5/chub.wsdl',
+			'inputFile' => 'http://esb.services.wisc.edu/esbv2/CHUB/WebService/chub-ws-'.$caosVersion.'/chub.wsdl',
 			'outputDir' => 'src/main/'
 	))	
 );
